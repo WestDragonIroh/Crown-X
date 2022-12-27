b02169c43c14dbcc8e36c7c85dfa129be653e0e9
Akshat <akshatjaiswal729@gmail.com>
2022-12-27 18:01:34 +0530

    2.1 added header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
 import Shop from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<Shop />} />
---
+++ src/components/header/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './header.scss'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <NavLink className='logo-container' to='/'>
+        <Logo className='logo' />
+        </NavLink>
+        <div className='options'>
+        <NavLink className='option' to='/shop'>
+            SHOP
+        </NavLink>
+        <NavLink className='option' to='/shop'>
+            CONTACT
+        </NavLink>
+        </div>
+    </div>
+  )
+}
